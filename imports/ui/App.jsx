753481cff00abe79c4473378691c73e8d41d829a
abelmaniquis <abelmaniquis@gmail.com>
2017-01-03 03:54:50 +0000

    list works

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,61 +1,49 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
+
 import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
+// App component - represents the whole app
 class App extends Component {
-  handleSubmit(event){
+  handleSubmit(event) {
     event.preventDefault();
+
+    // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-    
-    console.log(text);
-    
+
     Tasks.insert({
       text,
-      createdAt: new Date(), //Current time
+      createdAt: new Date(), // current time
     });
-    console.log(Tasks);
-    
+
+    // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
-  
-  renderTasks(){
-    return this.props.tasks.map((task)=>(
-      <Task key={task._id} task={task} />
-    ));
-  }
-  
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-      {_id: 4, text: 'a test element'}
-    ];
-  }
- 
+
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
- 
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
-        
-        <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
-          <input
-            type="text"
-            ref="textInput"
-            placeholder="Type to add new tasks"
-          />
-        </form>
+
         <ul>
           {this.renderTasks()}
         </ul>
@@ -64,13 +52,12 @@ class App extends Component {
   }
 }
 
-
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
-}
+};
 
-export default createContainer(()=>{
-  return{
+export default createContainer(() => {
+  return {
     tasks: Tasks.find({}).fetch(),
   };
 }, App);
